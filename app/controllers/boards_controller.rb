5cda64e96d6e6269159aff5c36c66ad114b599e9
Negar Kalbasi <neka@Negars-MacBook-Pro.local>
2015-11-13 13:41:06 -0500

    CRUD for boards and styling

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -5,8 +5,21 @@ class BoardsController < ApplicationController
   end
 
   #new
+  def new
+    @board = Board.new
+  end
 
   #create
+  def create
+    @board = current_user.boards.new(board_params)
+    if @board.save
+      flash[:notice] = "A board was successfully created"
+      redirect_to user_board_path(current_user, @board)
+    else
+      render :new
+    end
+
+  end
 
   #show
   def show
@@ -14,9 +27,25 @@ class BoardsController < ApplicationController
   end
 
   #edit
+  def edit
+    @board = Board.find(params[:id])
+  end
 
   #update
+  def update
+    @board = Board.find(params[:id])
+    if @board.update(board_params)
+      flash[:notice] = "A board was successfully updated"
+      redirect_to user_board_path(current_user, @board)
+    else
+      render :edit
+    end
+
+  end
 
   #destroy
-
+  private
+  def board_params
+    params.require(:board).permit(:title)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "boards#index"
+  root to: "welcome#index"
   resources :users do
     resources :boards do
       resources :pins do
